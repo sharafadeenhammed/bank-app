978d3c7820bde8b27517ae9c9b27764b8dc74d8d
Sharafadeen Hammed <sharafadeenhammed@yahoo.com>
2023-07-29 16:45:20 +0100

    fixed alignment issue on the homescreen

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,10 @@
 const Card = ({ title, description, imgSrc }) => {
   return (
     <div className="bg-light rounded my-card  m-3 d-flex flex-column align-items-center justify-content-center">
-      <div className="text-center">
-        <img
-          className="img-flush  img-resize d-block my-4"
-          src={imgSrc ? imgSrc : "./no-photo.jpg"}
-        />
-      </div>
+      <img
+        className="img-flush  img-resize d-block my-4"
+        src={imgSrc ? imgSrc : "./no-photo.jpg"}
+      />
       <h3 className=" text-succcess rounded p-2 bg-white text-success fw-600 fs-30 my-2">
         {title}
       </h3>
