599807e915d3973e31d1a165586076ef2762371e
Sharafadeen Hammed <sharafadeenhammed@yahoo.com>
2023-10-23 00:45:35 +0100

    cmpleted profile photo upload

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -32,7 +32,7 @@ const Profile = () => {
     try {
       // updating user data
       const res = await fetch(`${import.meta.env.VITE_BASE_URL}/user`, {
-        method: "POST",
+        method: "PUT",
         headers: {
           "Content-Type": "application/json",
         },
@@ -54,27 +54,36 @@ const Profile = () => {
   };
 
   const uploadPhoto = async (e) => {
+    // phot upload...
     e.preventDefault();
     const photo = new FormData();
     photo.append("photo", e.target.files[0]);
-    // console.log(photo);
-    // return;
+    const value = photo.values();
+    setIsLoading(true);
     try {
       // updating user data
-      const res = await fetch(`${import.meta.env.VITE_BASE_URL}/user/photo`, {
+      const res = await fetch(`${import.meta.env.VITE_BASE_URL}/user`, {
         method: "POST",
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
         credentials: "include",
         body: photo,
       });
+
       const data = await res.json();
-      console.log(data);
+      if (!res.ok) throw new Error(data.message);
+      userReducerDispatcher({ payload: data.data, type: "setuser" });
+      setUserData(data.data);
+      setIsLoading(false);
+      toast.success("profile photo upload sucesfully");
     } catch (err) {
       console.log(err);
+      if (err.message === "Failed to fetch") {
+        setIsLoading(false);
+        toast.success("photo upload erro check internet connection!");
+        return;
+      }
+      setIsLoading(false);
+      toast.error(err.message);
     }
-    console.log(photo);
   };
 
   return (
@@ -84,18 +93,28 @@ const Profile = () => {
       ) : (
         <>
           <div className="flex justify-content-center ">
-            <div className="d-flex flex-column ">
+            <div className="d-flex flex-column align-items-center ">
               <img
                 src={user.photo_url ?? noPhoto}
                 alt={`${user.first_name} ${user.last_name}`}
                 className="img-img-flush profile-img"
               />
-              <div className="d-flex flex-direction-row">
-                <form action="" onSubmit={uploadPhoto}></form>
-                <label htmlFor="photo"> upload photo</label>
+              <div className="d-flex align-items-center">
+                <label className="btn btn-secondary btn-sm" htmlFor="photo">
+                  change photo
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="1em"
+                    viewBox="0 0 512 512"
+                    className=" mx-2"
+                    fill="#fff"
+                  >
+                    <path d="M448 80c8.8 0 16 7.2 16 16V415.8l-5-6.5-136-176c-4.5-5.9-11.6-9.3-19-9.3s-14.4 3.4-19 9.3L202 340.7l-30.5-42.7C167 291.7 159.8 288 152 288s-15 3.7-19.5 10.1l-80 112L48 416.3l0-.3V96c0-8.8 7.2-16 16-16H448zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm80 192a48 48 0 1 0 0-96 48 48 0 1 0 0 96z" />
+                  </svg>
+                </label>
                 <input
                   onChange={uploadPhoto}
-                  // className="d-none"
+                  className="d-none"
                   type="file"
                   name="photo"
                   id="photo"
@@ -145,28 +164,28 @@ const Profile = () => {
                 </button>
               </div>
             )}
-            <div class="mb-3 row">
+            <div className="mb-3 row">
               <div className="col-sm-6 my-3">
-                <label htmlFor="first_name" class="form-label">
+                <label htmlFor="first_name" className="form-label">
                   First Name
                 </label>
                 <input
                   type="text"
                   disabled={!isEdit}
                   readOnly={!isEdit}
-                  class="form-control"
+                  className="form-control"
                   id="first_name"
                   value={userData?.first_name}
                   onChange={handleChange}
                 />
               </div>
               <div className="col-sm-6 my-3">
-                <label htmlFor="last_name" class="form-label">
+                <label htmlFor="last_name" className="form-label">
                   Last Name
                 </label>
                 <input
                   type="text"
-                  class="form-control"
+                  className="form-control"
                   disabled={!isEdit}
                   readOnly={!isEdit}
                   id="last_name"
@@ -176,14 +195,14 @@ const Profile = () => {
               </div>
             </div>
             {/* email and phone number */}
-            <div class="mb-3 row">
+            <div className="mb-3 row">
               <div className="col-sm-6 my-3">
-                <label htmlFor="email" class="form-label">
+                <label htmlFor="email" className="form-label">
                   Email
                 </label>
                 <input
                   type="text"
-                  class="form-control"
+                  className="form-control"
                   disabled={!isEdit}
                   readOnly={!isEdit}
                   id="email"
@@ -192,12 +211,12 @@ const Profile = () => {
                 />
               </div>
               <div className="col-sm-6 my-3">
-                <label htmlFor="phone" class="form-label">
+                <label htmlFor="phone" className="form-label">
                   Phone Number
                 </label>
                 <input
                   type="text"
-                  class="form-control"
+                  className="form-control"
                   disabled={!isEdit}
                   readOnly={!isEdit}
                   id="phone"
@@ -207,7 +226,7 @@ const Profile = () => {
               </div>
             </div>
             <div className="my-3">
-              <label htmlFor="address" class="form-label">
+              <label htmlFor="address" className="form-label">
                 Address
               </label>
               <input
@@ -219,7 +238,7 @@ const Profile = () => {
               />
             </div>
             {isEdit && (
-              <button type="submit" class="btn btn-success ">
+              <button type="submit" className="btn btn-success ">
                 submit profile update
               </button>
             )}
